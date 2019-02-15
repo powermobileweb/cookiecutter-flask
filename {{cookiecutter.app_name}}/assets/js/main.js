02fc33f17b4ba60cf99384866ec2fc57e2be21aa
Gruia Nichita <gruia@powermobile.org>
2019-02-15 20:47:44 +0100

    Bump pytest from 4.2.0 to 4.2.1 in /{{cookiecutter.app_name}}

---
+++ {{cookiecutter.app_name}}/assets/js/main.js
@@ -0,0 +1,15 @@
+/*
+ * Main Javascript file for {{cookiecutter.app_name}}.
+ *
+ * This file bundles all of your javascript together using webpack.
+ */
+
+// JavaScript modules
+require('jquery');
+require('font-awesome-webpack');
+require('popper.js');
+require('bootstrap');
+
+// Your own code
+require('./plugins.js');
+require('./script.js');
---
+++ {{cookiecutter.app_name}}/autoapp.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Create an application instance."""
+from {{cookiecutter.app_name}}.app import create_app
+
+app = create_app()
